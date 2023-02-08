9f01f1f453ddd9c3af6c2cfca6580210e3ad74ed
muratozkan79 <muratozkan79@gmail.com>
2023-02-08 15:55:33 +0300

    Some changes...

--- app.js
+++ app.js
@@ -1,21 +1,88 @@
-function refreshClock() {
-  let now = new Date();
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
-  let seconds = now.getSeconds();
+document.querySelector(".clock .hours").innerHTML = new Array(24)
+  .fill(0)
+  .map((n, i) => {
+    return `<div>${i < 10 ? "0" + i : i}</div>`;
+  })
+  .join("");
 
-  if (hours < 10) {
-    hours = "0" + hours;
-  }
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  }
+document.querySelector(".clock .minutes").innerHTML = new Array(60)
+  .fill(0)
+  .map((n, i) => {
+    return `<div>${i < 10 ? "0" + i : i}</div>`;
+  })
+  .join("");
 
-  let showClock = document.getElementById("clock");
-  showClock.innerText = hours + ":" + minutes + ":" + seconds;
+document.querySelector(".clock .seconds").innerHTML = new Array(60)
+  .fill(0)
+  .map((n, i) => {
+    return `<div>${i < 10 ? "0" + i : i}</div>`;
+  })
+  .join("");
+
+let time = new Date();
+let prevHours = time.getHours() * 100;
+let prevMinutes = time.getMinutes() * 100;
+let prevSeconds = time.getSeconds() * 100;
+
+document.querySelector(".clock .hours div").style.marginTop =
+  prevHours + "px";
+document.querySelector(".clock .minutes div").style.marginTop =
+  prevMinutes + "px";
+document.querySelector(".clock .seconds div").style.marginTop =
+  prevSeconds + "px";
+
+function updateClock() {
+  let time = new Date();
+  let hours = time.getHours() * 100;
+  let minutes = time.getMinutes() * 100;
+  let seconds = time.getSeconds() * 100;
+  if (prevHours !== hours) {
+    prevHours = hours;
+    document.querySelector(".clock .hours div").style.marginTop = hours + "px";
+    document.querySelector(".clock .hours").classList.add("animate");
+    setTimeout(function () {
+      document.querySelector(".clock .hours").classList.remove("animate");
+    }, 800);
+  }
+  if (prevMinutes !== minutes) {
+    prevMinutes = minutes;
+    document.querySelector(".clock .minutes div").style.marginTop = minutes + "px";
+    document.querySelector(".clock .minutes").classList.add("animate");
+    setTimeout(function () {
+      document.querySelector(".clock .minutes").classList.remove("animate");
+    }, 800);
+  }
+  if (prevSeconds !== seconds) {
+    prevSeconds = seconds;
+    document.querySelector(".clock .seconds div").style.marginTop = seconds + "px";
+    document.querySelector(".clock .seconds").classList.add("animate");
+    setTimeout(function () {
+      document.querySelector(".clock .seconds").classList.remove("animate");
+    }, 800);
+  }
 }
 
-setInterval(refreshClock, 1000);
+setInterval(updateClock, 1000);
+
+
+// function refreshClock() {
+//   let now = new Date();
+//   let hours = now.getHours();
+//   let minutes = now.getMinutes();
+//   let seconds = now.getSeconds();
+
+//   if (hours < 10) {
+//     hours = "0" + hours;
+//   }
+//   if (minutes < 10) {
+//     minutes = "0" + minutes;
+//   }
+//   if (seconds < 10) {
+//     seconds = "0" + seconds;
+//   }
+
+//   let showClock = document.getElementById("clock");
+//   showClock.innerText = hours + ":" + minutes + ":" + seconds;
+// }
+
+// setInterval(refreshClock, 1000);
